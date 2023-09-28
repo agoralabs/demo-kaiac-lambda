b783aaadc842338f14d10fa4acd329fcf7cf6e68
jekobokidou <joseph.ekobokidou@gmail.com>
2023-09-28 00:41:37 +0000

    Add Lambda function support for Backend

---
+++ hello.sh
@@ -0,0 +1,18 @@
+handler () {
+    set -e
+
+    # Event Data is sent as the first parameter
+    EVENT_DATA=$1
+
+    # This is the Event Data
+    echo $EVENT_DATA
+
+    # Example of command usage
+    EVENT_JSON=$(echo $EVENT_DATA | jq .)
+
+    # Example of AWS command that's output will show up in CloudWatch Logs
+    aws s3 ls
+
+    # This is the return value because it's being sent to stderr (>&2)
+    echo "{\"success\": true}" >&2
+}
--- index.sh
+++
@@ -1,6 +0,0 @@
-handler () {
-    set -e
-    EVENT_DATA=$1
-    aws s3 ls $(echo $EVENT_DATA | jq ."bucket")
-    echo "{\"success\": true}" >&2
-}
