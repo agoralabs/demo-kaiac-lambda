561c956c2ab5b5060b2ea86481583fd8248f986b
jekobokidou <joseph.ekobokidou@gmail.com>
2023-09-30 23:09:48 +0000

    Add Lambda support for Backend

--- launch.js
+++ launch.js
@@ -1,23 +1,56 @@
-// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-var aws = require("aws-sdk");
-var ses = new aws.SES({ region: "us-west-2" });
-exports.handler = async function (event) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-  var params = {
-    Destination: {
-      ToAddresses: ["joseph.ekobokidou@gmail.com"],
-    },
-    Message: {
-      Body: {
-        Text: { Data: "Nom: "+event.username + "\n" + "Email: "+event.useremail + "\n" + "Tel: "+event.userphone + "\n" + "Message: "+event.usermsg + "\n"},
-      },
-
-      Subject: { Data: "[AGORALABS_WEB_SITE] "+event.usersubject },
-    },
-    Source: "agora.labs.contact@gmail.com",
+exports.handler = function(event, context) {
+  
+    // Load the AWS SDK for Node.js
+    var AWS = require('aws-sdk');
+    // Set the region 
+    AWS.config.update({region: 'eu-west-3'});
+    
+    // Create EC2 service object
+    var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+    
+    var params = {
+      InstanceIds: ["i-06b6843ab6c4ac9bf"],
+      DryRun: true
+    };
+    
+    
+    if (event.command === "START") {
+      // Call EC2 to start the selected instances
+      ec2.startInstances(params, function(err, data) {
+        console.log("Starting", data);
+        if (err && err.code === 'DryRunOperation') {
+          params.DryRun = false;
+          ec2.startInstances(params, function(err, data) {
+              if (err) {
+                context.fail(err);
+                console.log("Error", err);
+              } else if (data) {
+                context.succeed("Success");
+                console.log("Success", data.StartingInstances);
+              }
+          });
+        } else {
+          console.log("You don't have permission to start instances.");
+        }
+      });
+    } else if (event.command === "STOP") {
+      // Call EC2 to stop the selected instances
+      ec2.stopInstances(params, function(err, data) {
+        if (err && err.code === 'DryRunOperation') {
+          params.DryRun = false;
+          ec2.stopInstances(params, function(err, data) {
+              if (err) {
+                context.fail(err);
+                console.log("Error", err);
+              } else if (data) {
+                context.succeed("Success");
+                console.log("Success", data.StoppingInstances);
+              }
+          });
+        } else {
+          context.fail(err);
+          console.log("You don't have permission to stop instances");
+        }
+      });
+    }
   };
- 
-  return ses.sendEmail(params).promise()
-};
