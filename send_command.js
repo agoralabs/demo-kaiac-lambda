8a6f55f261cf8ceace8ac09c3071fde431c5bd2b
jekobokidou <joseph.ekobokidou@gmail.com>
2023-10-05 07:06:54 +0000

    Add Lambda support for Backend

--- send_command.js
+++ send_command.js
@@ -1,30 +1,9 @@
 // Load the AWS SDK for Node.js
 const AWS = require('aws-sdk');
 
-// Set the region 
-AWS.config.update({region: 'eu-west-3'});
-
-// Create EC2 service object
-const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
-const SSM = new AWS.SSM();
-
-
-async function getInstanceId() {
+async function getInstanceId(filters) {
     var describeParams = { 
-        Filters: [
-            {
-                Name:"tag:Deployment",
-                Values: [
-                    "backend-alfresco-staging"
-                ]
-            },
-            {
-                Name:"instance-state-name",
-                Values: [
-                    "running","pending","stopped"
-                ]
-            }
-        ]
+        Filters: filters
     };
 
     try {
@@ -80,10 +59,10 @@ async function startAutomationExecution(instanceId, ssmDocument) {
       var executionId = data.AutomationExecutionId;
 
       return executionId;
-  } catch (err) {
-      console.error('Erreur lors du démarrage de l\'exécution d\'automatisation:', err);
-      throw err; // Propagez l'erreur vers le code appelant si nécessaire
-  }
+    } catch (err) {
+        console.error('Erreur lors du démarrage de l\'exécution d\'automatisation:', err);
+        throw err; // Propagez l'erreur vers le code appelant si nécessaire
+    }
 
 }
 
@@ -92,18 +71,17 @@ async function getAutomationExecutionStatus(executionId){
       AutomationExecutionId: executionId, // L'ID de l'exécution d'automatisation à récupérer
     };
     
-    var status = "";
+    try {
+      const data = await SSM.getAutomationExecution(params).promise();
+      
+      var status = data.AutomationExecution.AutomationExecutionStatus;
 
-    await SSM.getAutomationExecution(params, (err, data) => {
-      if (err) {
-        console.error('Erreur lors de la récupération de l\'exécution d\'automatisation:', err);
-      } else {
-        console.log('Statut de l\'exécution d\'automatisation:', data.AutomationExecution.AutomationExecutionStatus);
-        status = data.AutomationExecution.AutomationExecutionStatus;
-      }
-    }).promise();
+      return status;
+    } catch (err) {
+        console.error('Erreur lors de la récupération du statut d\'automatisation:', err);
+        throw err; // Propagez l'erreur vers le code appelant si nécessaire
+    }
 
-    return status;
 }
 
 async function sendKaiacCommand(instances, commandName){
@@ -115,18 +93,17 @@ async function sendKaiacCommand(instances, commandName){
     },
   };
 
-  var commandId = "";
+  try {
+    const data = await SSM.sendCommand(params).promise();
+    
+    var commandId = data.Command.CommandId;
 
-  await SSM.sendCommand(params, (err, data) => {
-    if (err) {
+    return commandId;
+  } catch (err) {
       console.error('Erreur lors de l\'envoi de la commande:', err);
-    } else {
-      console.log('Commande envoyée avec succès. Command ID:', data.Command.CommandId);
-      commandId = data.Command.CommandId;
-    }
-  }).promise();
+      throw err; // Propagez l'erreur vers le code appelant si nécessaire
+  }
 
-  return commandId;
 }
 
 async function getCommandInvocationStatus(commandId, instanceId){
@@ -135,31 +112,55 @@ async function getCommandInvocationStatus(commandId, instanceId){
     InstanceId: instanceId
   };
 
-  var status = "";
+  try {
+    const data = await SSM.getCommandInvocation(params).promise();
+    
+    var status = data.Status;
 
-  await SSM.getCommandInvocation(params, (err, data) => {
-    if (err) {
+    return status;
+  } catch (err) {
       console.error('Erreur lors de la recupération du statut:', err);
-    } else {
-      console.log('Statut récupéré avec succès:', data.Status);
-      status = data.Status;
-    }
-  }).promise();
+      throw err; // Propagez l'erreur vers le code appelant si nécessaire
+  }
 
-  return status;
 }
 
 
 exports.handler = async(event) => {
   
-
     var body = JSON.parse(event['body']);
     var command = body.command;
+    var awsRegion = body.awsRegion;
+    // Set the region 
+    AWS.config.update({region: awsRegion});
+
+    // Create EC2 service object
+    const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+
+    // Create SSM service object
+    const SSM = new AWS.SSM();
 
     if (command == "GET_INSTANCE_ID") {
       try {
           
-          const instances = await getInstanceId();
+          var tagName = body.tagName;
+          var tagPrefixValue = body.tagPrefixValue;
+
+          var filters = [
+            {
+                Name: tagName,
+                Values: [
+                  tagPrefixValue
+                ]
+            },
+            {
+                Name:"instance-state-name",
+                Values: [
+                    "running","pending","stopped"
+                ]
+            }
+          ]
+          const instances = await getInstanceId(filters);
 
           if(instances.length == 0){
 
@@ -181,6 +182,106 @@ exports.handler = async(event) => {
           return {
               statusCode: 500,
               body: JSON.stringify({message: "Erreur lors de l\'arrêt de l\'instance EC2."})
+          };
+          
+      }
+    }
+
+
+
+    if (command == "START_SSM_AUTOMATION") {
+      try {
+        
+          var instanceId = body.instanceId;
+          var ssmDocument = body.ssmDocument;
+
+          const executionId = await startAutomationExecution(instanceId, ssmDocument);
+
+          return {
+            statusCode: 200,
+            body: JSON.stringify({command: command, executionId: executionId, message: "Automation exécutée avec succès."})
+          };
+
+      } catch (error) {
+          console.error(`Erreur lors de l\'execution de l\'automation : ${error}`);
+          
+          return {
+              statusCode: 500,
+              body: JSON.stringify({message: `Erreur lors de l\'execution de l\'automation : ${error}`})
+          };
+          
+      }
+    }
+
+
+    if (command == "STATUS_SSM_AUTOMATION") {
+      try {
+        
+          var executionId = body.executionId;
+
+          const status = await getAutomationExecutionStatus(executionId);
+
+          return {
+            statusCode: 200,
+            body: JSON.stringify({command: command, executionStatus: status, message: "Statut récupéré avec succès."})
+          };
+
+      } catch (error) {
+          console.error(`Erreur lors de la récupération du statut de l\'automation : ${error}`);
+          
+          return {
+              statusCode: 500,
+              body: JSON.stringify({message: `Erreur lors de la récupération du statut de l\'automation : ${error}`})
+          };
+      }
+    }
+  
+
+
+    if (command == "SEND_KAIAC_COMMAND") {
+      try {
+        
+          var instanceId = body.instanceId;
+          var instances = [instanceId];
+          var kaiacCommand = body.kaiacCommand;
+
+          const commandId = await sendKaiacCommand(instances, kaiacCommand);
+          
+          return {
+            statusCode: 200,
+            body: JSON.stringify({commandId: commandId, message: "Commande lancée avec succès."})
+          };
+
+      } catch (error) {
+          console.error(`Erreur lors du lancement de la commande : ${error}`);
+
+          return {
+              statusCode: 500,
+              body: JSON.stringify({message: `Erreur lors du lancement de la commande : ${error}`})
+          };
+          
+      }
+    }
+
+    if (command == "STATUS_KAIAC_COMMAND") {
+      try {
+
+          var instanceId = body.instanceId;
+          var commandId = body.commandId;
+
+          const status = await getCommandInvocationStatus(commandId, instanceId);
+
+          return {
+            statusCode: 200,
+            body: JSON.stringify({commandStatus: status, message: "Statut de la commande avec succès."})
+          };
+
+      } catch (error) {
+          console.error(`Erreur lors de la récupération du statut de la commande : ${error}`);
+
+          return {
+              statusCode: 500,
+              body: JSON.stringify({message: `Erreur lors de la récupération du statut de la commande : ${error}`})
           };
           
       }
@@ -208,55 +309,7 @@ exports.handler = async(event) => {
           
       }
     }
-
-    if (command == "START_SSM_AUTOMATION") {
-      try {
-        
-          var instanceId = body.instanceId;
-          var ssmDocument = body.ssmDocument;
-
-          const executionId = await startAutomationExecution(instanceId, ssmDocument);
-
-          return {
-            statusCode: 200,
-            body: JSON.stringify({executionId: executionId, message: "Automation exécutée avec succès."})
-          };
-
-      } catch (error) {
-          console.error(`Erreur lors de l\'execution de l\'automation : ${error}`);
-          
-          return {
-              statusCode: 500,
-              body: JSON.stringify({message: `Erreur lors de l\'execution de l\'automation : ${error}`})
-          };
-          
-      }
-    }
-
-
-    if (command == "STATUS_SSM_AUTOMATION") {
-      try {
-        
-          var executionId = body.executionId;
-
-          var status = getAutomationExecutionStatus(executionId);
-
-          return {
-            statusCode: 200,
-            body: JSON.stringify({executionId: executionId, message: "Automation exécutée avec succès."})
-          };
-
-      } catch (error) {
-          console.error(`Erreur lors de l\'execution de l\'automation : ${error}`);
-          
-          return {
-              statusCode: 500,
-              body: JSON.stringify({message: "Erreur lors de l\'execution de l\'automation."})
-          };
-          
-      }
-    }
-  
+    
     if (command == "START_EC2") {
       try {
         
@@ -280,56 +333,9 @@ exports.handler = async(event) => {
       }
     }
 
-    if (command == "KAIAC_COMMAND") {
-      try {
-        
-          var instanceId = body.instanceId;
-          var instances = [instanceId];
-          var kaiacCommand = body.kaiacCommand;
-
-          var commandId = sendKaiacCommand(instances, kaiacCommand);
-          
-          return {
-            statusCode: 200,
-            body: JSON.stringify({commandId: commandId, message: "Commande lancée avec succès."})
-          };
-
-      } catch (error) {
-          console.error(`Erreur lors du lancement de la commande : ${error}`);
-
-          return {
-              statusCode: 500,
-              body: JSON.stringify({message: "Erreur lors du lancement de la commande."})
-          };
-          
-      }
-    }
-
-    if (command == "STATUS_KAIAC_COMMAND") {
-      try {
-
-          var instanceId = body.instanceId;
-          var commandId = body.commandId;
-          var status = getCommandInvocationStatus(commandId, instanceId);
-          return {
-            statusCode: 200,
-            body: JSON.stringify({status: status, message: "Statut de la commande avec succès."})
-          };
-
-      } catch (error) {
-          console.error(`Erreur lors de la récupération du statut de la commande : ${error}`);
-
-          return {
-              statusCode: 500,
-              body: JSON.stringify({message: "Erreur lors de la récupération du statut de la commande."})
-          };
-          
-      }
-    }
-
     var response = {
-        message: "Invalid command",
-        context: command
+        command: command,
+        message: "Invalid command"
     };
     
     return {
